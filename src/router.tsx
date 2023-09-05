fcce27b4408418798da0449b7876c0f5e35e86f8
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-09-05 09:37:58 +0200

    add contact page

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from './components/Layout';
+import { TableReservations } from './components/TableReservations';
+import { Contact } from './components/pages/Contact';
 import StartPage from './components/pages/StartPage';
 
 export const router = createBrowserRouter([
@@ -11,6 +13,14 @@ export const router = createBrowserRouter([
 				path: '/',
 				element: <StartPage />,
 				index: true,
+			},
+			{
+				path: '/booking',
+				element: <TableReservations />,
+			},
+			{
+				path: '/contact',
+				element: <Contact />,
 			},
 		],
 	},
