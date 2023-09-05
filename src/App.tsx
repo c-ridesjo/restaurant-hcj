631b61c70ce1a015cb93386f10b40651800b5f3c
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-09-05 09:27:12 +0200

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
+
 function App() {
-  return (
-    <>
-    </>
-  )
+	return (
+		<>
+			<RouterProvider router={router}></RouterProvider>
+		</>
+	);
 }
 
-export default App
+export default App;
