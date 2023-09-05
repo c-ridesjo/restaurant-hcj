5963552dde868f21611779e3acaab27b132aa048
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-09-05 09:34:18 +0200

    router imports

--- src/components/pages/StartPage.tsx
+++ src/components/pages/StartPage.tsx
@@ -1,12 +1,9 @@
-import React from "react";
+import React from 'react';
 
-const StartPage: React.FC = () => {
-  return (
-    <p>
-      Welcome to our exclusive restaurant with specialties from
-      the sea!
-    </p>
-  );
+export const StartPage: React.FC = () => {
+	return (
+		<p>Welcome to our exclusive restaurant with specialties from the sea!</p>
+	);
 };
 
 export default StartPage;
--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout';
+import StartPage from './components/pages/StartPage';
 
 export const router = createBrowserRouter([
 	{
