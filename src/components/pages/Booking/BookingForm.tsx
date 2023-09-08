bbc2e866c8ba1fe41e66af46509c7e26dc83857f
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-09-08 11:12:36 +0200

    add bookingForm - obs not finnished

--- src/components/pages/Booking/BookingForm.tsx
+++ src/components/pages/Booking/BookingForm.tsx
@@ -1,7 +1,60 @@
+import type { FieldValues } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+
 export const BookingForm = () => {
-  return (
-    <>
-      BookingForm
-    </>
-  );
-}
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isSubmitting },
+		reset,
+		getValues,
+	} = useForm();
+
+	const onSubmit = (data: FieldValues) => {
+		reset();
+	};
+
+	return (
+		<>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<input
+					{...register('firstname', {
+						required: 'Firstname is required',
+						minLength: {
+							value: 2,
+							message: 'Firstname has to be 2 characters long',
+						},
+					})}
+					type='text'
+					placeholder='Firstname'
+				/>
+				<input
+					{...register('lastname', {
+						required: 'Lastname is required',
+						minLength: {
+							value: 2,
+							message: 'Lastname has to be 2 characters long',
+						},
+					})}
+					type='text'
+					placeholder='Lastname'
+				/>
+				<input
+					{...register('email', { required: 'Email is required' })}
+					type='email'
+					placeholder='Email'
+				/>
+				<input
+					{...register('phone', {
+						required: 'You need to provide a phone number',
+					})}
+					type='tel'
+					placeholder='Phone number'
+				/>
+				<button type='submit' disabled={isSubmitting}>
+					Book table
+				</button>
+			</form>
+		</>
+	);
+};
